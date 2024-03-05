ed38d5b5a2015e9f3dcb92175de28e137d70e5bc
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-05 22:19:39 +0530

    rem

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,22 +2,19 @@ import { Suspense } from "react"
 import Footer from "../compnents/Footer";
 import Header from "../compnents/Header";
 import { Outlet } from "react-router-dom";
-import AuthProvider from "../providers/AuthProvider";
 import AnimeProvider from "../providers/AnimeProvider";
 
 const MainLayout = () => {
     return (
-        <AuthProvider>
-            <AnimeProvider>
-                <Header />
-                <div className="min-h-[calc(100vh-45px)] sm:mt-[120px] mt-[140px]">
-                    <Suspense>
-                        <Outlet />
-                    </Suspense>
-                </div>
-                <Footer />
-            </AnimeProvider>
-        </AuthProvider>
+        <AnimeProvider>
+            <Header />
+            <div className="min-h-[calc(100vh-45px)] sm:mt-[120px] mt-[140px]">
+                <Suspense>
+                    <Outlet />
+                </Suspense>
+            </div>
+            <Footer />
+        </AnimeProvider>
     );
 }
 
