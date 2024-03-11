65f496ebe7c2e58acf465755c6f3dcd44d15eaf8
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-11 09:16:57 +0530

    mistake fixed

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 
 export default function AuthLayout() {
     return (
         <section className="grid w-full min-h-[100dvh] place-content-center">
-            <Outlet />
+            <Suspense>
+                <Outlet />
+            </Suspense>
         </section>
     )
 }
